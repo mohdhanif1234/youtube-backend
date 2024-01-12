99e6ceec1944596a001f24c654b057d617b0f814
MohdHanif Topia <mohdhanif.topia@nw18.com>
2024-01-12 22:51:06 +0530

    fixed all user controller bugs

--- src/controllers/user.controller.js
+++ src/controllers/user.controller.js
@@ -15,7 +15,7 @@ const registerUser = asyncHandler(async (req, res) => {
     }
 
     // check if user already exists by username or email
-    const existedUser = User.findOne({
+    const existedUser = await User.findOne({
         $or: [{ username }, { email }]
     })
 
@@ -26,7 +26,12 @@ const registerUser = asyncHandler(async (req, res) => {
     // check for images, check for avatar
     console.log(req.files)
     const avatarLocalPath = req.files?.avatar[0]?.path;
-    const coverImageLocalPath = req.files?.coverImage[0]?.path
+    // const coverImageLocalPath = req.files?.coverImage[0]?.path
+
+    let coverImageLocalPath;
+    if (req.files && Array.isArray(req.files.coverImage) && req.files.coverImage.length > 0) {
+        coverImageLocalPath = req.files.coverImage[0].path
+    }
 
     if (!avatarLocalPath) {
         throw new ApiError(400, 'Avatar file is required');
